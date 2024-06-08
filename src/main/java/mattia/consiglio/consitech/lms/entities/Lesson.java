2b2cc6079806e28cd60be16de93e307c8e6faee3
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-06-08 19:21:37 +0200

    chore: Add column definition for liveEditor in Lesson entity and add JsonView annotations in Media entity

--- src/main/java/mattia/consiglio/consitech/lms/entities/Lesson.java
+++ src/main/java/mattia/consiglio/consitech/lms/entities/Lesson.java
@@ -14,6 +14,7 @@ import mattia.consiglio.consitech.lms.utils.View;
 @Table(name = "lessons")
 
 public class Lesson extends AbstractContent {
+    @Column(columnDefinition = "TEXT")
     @JsonView(View.Internal.class)
     private String liveEditor;
 
--- src/main/java/mattia/consiglio/consitech/lms/entities/Media.java
+++ src/main/java/mattia/consiglio/consitech/lms/entities/Media.java
@@ -1,11 +1,13 @@
 package mattia.consiglio.consitech.lms.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import mattia.consiglio.consitech.lms.utils.View;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -21,19 +23,27 @@ public class Media {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Setter(value = AccessLevel.NONE)
+    @JsonView(View.Public.class)
     private UUID id;
     @Column(nullable = false)
+    @JsonView(View.Public.class)
     private String url;
     @Column(nullable = false)
+    @JsonView(View.Public.class)
     private MediaType type;
+    @JsonView(View.Public.class)
     private String alt;
+    @JsonView(View.Public.class)
     private int width;
+    @JsonView(View.Public.class)
     private int height;
     @JsonIgnore
     private String cloudinaryPublicId;
+    @JsonView(View.Public.class)
     private String mainColor;
     @JsonIgnore
     private String hash;
+    @JsonView(View.Public.class)
     private LocalDateTime uploadedAt;
     @JsonIgnore
     @OneToMany(mappedBy = "thumbnail")
