78644e17bfd0e9673070eb9ac963cbd2f239ec88
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-08-21 13:43:37 +0200

    refactor(ServerConfig): format allowedHosts list
    - Split allowedHosts list into multiple lines
    - Improve readability by aligning list elements

--- src/main/java/mattia/consiglio/consitech/lms/config/ServerConfig.java
+++ src/main/java/mattia/consiglio/consitech/lms/config/ServerConfig.java
@@ -18,7 +18,10 @@ public class ServerConfig {
 
     @Bean("allowedHosts")
     public List<String> allowedHosts() {
-        return Arrays.asList("http://localhost:3000", "https://lms.consitech.it", "https://consitech-lms-front-end.vercel.app", "https://consitech-lms-front-end-git-develop-mattias-projects-67143469.vercel.app/");
+        return Arrays.asList("http://localhost:3000",
+                "https://lms.consitech.it",
+                "https://consitech-lms-front-end.vercel.app",
+                "https://consitech-lms-front-end-git-develop-mattias-projects-67143469.vercel.app");
     }
 
     @Bean("mediaPath")
